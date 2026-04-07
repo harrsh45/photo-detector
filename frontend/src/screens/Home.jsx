088fd80cb17f2a0c94c4c421ece0a3ef4cfed66d
harrsh45 <harrshpadhiyar@gmail.com>
2026-04-07 19:33:02 +0530

    l

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -126,7 +126,7 @@ const Home = () => {
       setFilePreview(null)
       setUploadProgress(0)
       
-      // Refresh media l
+      // Refresh media 
       fetchMedia()
     } catch (err) {
       console.error('Upload error:', err)
